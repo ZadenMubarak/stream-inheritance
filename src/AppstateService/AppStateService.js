56a7eb5be9b0047ce56bae3960205905f1872c38
ZadenMubarak <zaden.ngobeni@gmail.com>
2023-12-06 19:56:05 +0200

    startet to call the database

--- src/AppstateService/AppStateService.js
+++ src/AppstateService/AppStateService.js
@@ -1,3 +1,5 @@
+import { Polybase } from "@polybase/client";
+import { Auth } from "@polybase/auth";
 
 export class AppStateService{
     constructor() {
@@ -8,7 +10,51 @@ export class AppStateService{
         AppStateService.instance = this;
 
         this.walletAddress = '';
-        this.connected = false
+        this.connected = false;
+
+        const db = new Polybase({
+            defaultNamespace: "pk/0xbd242ce427525d219c617b9856f0052b52334321d47d1793a7653cab5b2dac45792735a33e4b2789cbf8063555816d8a37226f8b393645c78244c175a010fbed/stream-inherritance",
+        });
+
+        this.polyBaseResponse = [];
+        this.nextPolybaseRecordID = null;
+        this.collectionReference = db.collection('StreamInherritance');
+
+    }
+
+    
+    generatePolybaseID = () => {
+        this.nextPolybaseRecordID = this.polybaseResponse.length + 1;
+        return this.nextPolybaseRecordID.toString();
+    }
+
+    async getItemsFromRecord () {
+        this.getUseAddress();
+        await this.collectionReference.get().then((data)=>{
+            let array = data.data;
+            let temp = []  
+            array.forEach(element => {
+                temp.push(element.data)
+            });
+            console.log(temp[0].author);
+            console.log("lenth: ", temp.length);
+            this.polybaseResponse = temp;
+            // console.log('polybase response: ', this.polybaseResponse);
+            return temp;
+        }).catch((error)=>{
+            console.log(error)
+        });
+    }
+
+    async createProject(projectObject){
+        let id = this.generatePolybaseID()
+        await this.collectionReference.create([
+            id,
+            projectObject.streamCreatorName,
+            projectObject.benficiaryName,
+            projectObject.beneficiaryDetails,
+            projectObject.numberOfMonthsToStream,
+        ])
     }
 
     connectToMetaMask = async ()=> {
